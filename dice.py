d28f7315b828fa8bd0e471778051edeee1bca3e8
teverist <tobyeverist1@gmail.com>
2022-02-02 21:09:29 +0000

    Added display of ASCII art

--- dice.py
+++ dice.py
@@ -13,11 +13,31 @@ def parse_input(input):
 
 def roll_dice(num_die):
     roll_results = []
-    for _ in range(num_die):
+    for _ in range(num_dice):
         roll = random.randint(1,6)
         roll_results.append(roll)
 
     return roll_results
+
+def generate_dice_faces(dice_values):
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
 
 DICE_ART = {
 
@@ -90,8 +110,14 @@ DIE_WIDTH = len(DICE_ART[1][0])
 DIE_FACE_SEPARATOR = " "
 
 
-# Main functionality
-num_dice_input = input("How many dice would you like to roll? [1-6]. (Type exit to quit): ")
+# 1. Get and validate user's input
+num_dice_input = input("How many dice do you want to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
+# 2. Roll the dice
+roll_results = roll_dice(num_dice)
+# 3. Generate the ASCII diagram of dice faces
+dice_face_diagram = generate_dice_faces(roll_results)
+# 4. Display the diagram
+print(f"\n{dice_face_diagram}")
 
 
