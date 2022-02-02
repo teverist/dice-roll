b8558027b5d2eede31dcd59e3c31b7b5a91b4d14
teverist <tobyeverist1@gmail.com>
2022-02-02 21:11:58 +0000

    Refactored code. Added helper functions to atomise code, each function has singular reponsibility

--- dice.py
+++ dice.py
@@ -19,11 +19,37 @@ def roll_dice(num_die):
 
     return roll_results
 
-def generate_dice_faces(dice_values):
+def generate_dice_faces_diagram(dice_values):
+    """Return an ASCII diagram of dice faces from `dice_values`.
+
+    The string returned contains an ASCII representation of each die.
+    For example, if `dice_values = [4, 1, 3, 2]` then the string
+    returned looks like this:
+
+    ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
+    ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
+    │  ●   ●  │ │         │ │  ●      │ │  ●      │
+    │         │ │    ●    │ │    ●    │ │         │
+    │  ●   ●  │ │         │ │      ●  │ │      ●  │
+    └─────────┘ └─────────┘ └─────────┘ └─────────┘
+    """
+    dice_faces = _get_dice_faces(dice_values)
+    dice_faces_rows = _generate_dice_faces_rows(dice_faces)
+
+    # Generate header with the word "RESULTS" centered
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
+def _get_dice_faces(dice_values):
     dice_faces = []
     for value in dice_values:
         dice_faces.append(DICE_ART[value])
+    return dice_faces
 
+def _generate_dice_faces_rows(dice_faces):
     dice_faces_rows = []
     for row_idx in range(DIE_HEIGHT):
         row_components = []
@@ -31,13 +57,7 @@ def generate_dice_faces(dice_values):
             row_components.append(die[row_idx])
         row_string = DIE_FACE_SEPARATOR.join(row_components)
         dice_faces_rows.append(row_string)
-
-    width = len(dice_faces_rows[0])
-    diagram_header = " RESULTS ".center(width, "~")
-
-    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
-    return dice_faces_diagram
-
+    return dice_faces_rows
 
 DICE_ART = {
 
@@ -116,7 +136,7 @@ num_dice = parse_input(num_dice_input)
 # 2. Roll the dice
 roll_results = roll_dice(num_dice)
 # 3. Generate the ASCII diagram of dice faces
-dice_face_diagram = generate_dice_faces(roll_results)
+dice_face_diagram = generate_dice_faces_diagram(roll_results)
 # 4. Display the diagram
 print(f"\n{dice_face_diagram}")
 
