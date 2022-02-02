05e716d5c81ae1ffca0f6a92cb3ced735013428a
teverist <tobyeverist1@gmail.com>
2022-02-02 21:03:12 +0000

    Added ASCII art of die and roll dice functionality

--- dice.py
+++ dice.py
@@ -1,7 +1,6 @@
 import random
 
 
-# Main functionality
 def parse_input(input):
     if input.strip() in {"1", "2", "3", "4", "5", "6"}:
         return int(input)
@@ -12,8 +11,87 @@ def parse_input(input):
         num_dice_input
 
 
+def roll_dice(num_die):
+    roll_results = []
+    for _ in range(num_die):
+        roll = random.randint(1,6)
+        roll_results.append(roll)
+
+    return roll_results
+
+DICE_ART = {
+
+    1: (
+
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+
+    ),
+
+    2: (
+
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+
+    ),
+
+    3: (
+
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+
+    ),
+
+    4: (
+
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+
+    ),
+
+    5: (
+
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+
+    ),
+
+    6: (
+
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+
+    ),
+
+}
+
+DIE_HEIGHT = len(DICE_ART[1])
+
+DIE_WIDTH = len(DICE_ART[1][0])
+
+DIE_FACE_SEPARATOR = " "
+
+
+# Main functionality
 num_dice_input = input("How many dice would you like to roll? [1-6]. (Type exit to quit): ")
 num_dice = parse_input(num_dice_input)
-
 
 
